ba2f42fc5e8ef5761b4294e455b8b08ffca58f4c
maxk <maxk@corp.doctor.com>
2020-04-15 20:28:13 -0700

    Initial Commit

---
+++ CompletionistTests/CompletionistTests.swift
@@ -0,0 +1,34 @@
+//
+//  CompletionistTests.swift
+//  CompletionistTests
+//
+//  Created by Max Kerscher-Santelli on 4/15/20.
+//  Copyright © 2020 Max Kerscher-Santelli. All rights reserved.
+//
+
+import XCTest
+@testable import Completionist
+
+class CompletionistTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
